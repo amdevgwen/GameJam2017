4e6ba057d817fca12a8c18012c80edd279ff43cf
Stan <stantsai9@gmail.com>
2017-01-21 01:22:07 -0800

    Working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
