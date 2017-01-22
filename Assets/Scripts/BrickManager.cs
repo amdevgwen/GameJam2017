bf19f4c810d9508ea643ad88be3228a0c682d428
Nariera <angelofangle@gmail.com>
2017-01-22 06:54:13 -0800

    New Water System

---
+++ Assets/Scripts/BrickManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickManager : MonoBehaviour
+{
+
+    public GameObject Origin;
+    public GameObject BrickPrefab;
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 OriginPosition = Origin.transform.position;
+        GameObject Previous = Origin;
+        for(int i = 0; i< 40; i++)
+        {
+            Vector3 NewPos = OriginPosition;
+            NewPos.x += (i + 1) * 4;
+            GameObject Current = Instantiate(BrickPrefab, NewPos,Quaternion.identity);
+
+            SpringJoint2D ac = Current.AddComponent<SpringJoint2D>();
+            ac.connectedBody = Previous.GetComponent<Rigidbody2D>();
+            //ac.autoConfigureConnectedAnchor = true;
+            ac.autoConfigureConnectedAnchor = false;
+            ac.autoConfigureDistance = false;
+            ac.dampingRatio = 0.1f;
+            Previous = Current;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/BrickScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour {
+
+    public Rigidbody2D body;
+
+    public bool Moveable = false;
+
+    private float initX;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+        initX = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float speed = 25f;
+        if (Moveable)
+        {
+            //body.velocity = new Vector3(0, Input.GetAxis("Vertical") * speed, 0);
+            Vector3 pos = transform.position;
+            pos.y += Input.GetAxis("Vertical") * speed;
+            transform.position = pos;
+        }
+
+        Vector3 position = transform.position;
+        position.x = initX;
+        transform.position= position;
+    }
+}
