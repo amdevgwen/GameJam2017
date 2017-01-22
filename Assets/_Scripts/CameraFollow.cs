40e2f738f4f965fd3bb8799fae30aaa3d8a1f467
Alec Asperslag <aaspersl@ucsc.edu>
2017-01-21 17:50:00 -0800

    Look, a camera script
    You can put this on Cameras now.  It will slowly move towards where the
    ship is.  More features coming soon.

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform camTarget;
+    public Vector2 Offset;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 targetPos = new Vector3(camTarget.position.x+Offset.x, camTarget.position.y+Offset.y, transform.position.z);
+        Vector3 movement = (targetPos-transform.position)*Time.deltaTime;
+        transform.position += movement;
+	}
+}
