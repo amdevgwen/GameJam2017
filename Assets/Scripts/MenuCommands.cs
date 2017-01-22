00862abfb59c38fc2f31e0e9a38f8a93cc4ada73
Stralor <github@patrick-scott.com>
2017-01-21 23:56:08 -0800

    Maybe Networking?

---
+++ Assets/Scripts/MenuCommands.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MenuCommands : MonoBehaviour
+{
+	NetworkManager _netMan;
+	NetworkDiscovery _netDisc;
+	public InputField hostAddressField;
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	public void SetIPAndJoin()
+	{
+		_netMan.networkPort = 7777;
+		if (hostAddressField != null && hostAddressField.text != null && hostAddressField.text.Trim() != "")
+		{
+			_netMan.networkAddress = hostAddressField.text.Trim();
+
+			_netMan.StartClient();
+
+			Text ipText = GameObject.Find("Host IP").GetComponent<Text>();
+			ipText.text = hostAddressField.text.Trim();
+		}
+	}
+
+	public void JoinLocal()
+	{
+		_netDisc.StartAsClient();
+	}
+
+	public void Host()
+	{
+		_netMan.networkPort = 7777;
+
+		Text ipText = GameObject.Find("Host IP").GetComponent<Text>();
+		ipText.text = Network.player.ipAddress + " (Host)";
+
+		_netDisc.StartAsServer();
+		_netMan.StartHost();
+	}
+
+	void Start()
+	{
+		_netMan = FindObjectOfType<NetworkManager>();
+		_netDisc = FindObjectOfType<NetworkDiscovery>();
+
+		_netDisc.Initialize();
+	}
+}
