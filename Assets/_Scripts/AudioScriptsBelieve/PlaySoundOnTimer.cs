6061c9d0a5e8be4361f6b215d873e8f04464b8c7
Alec Asperslag <aaspersl@ucsc.edu>
2017-01-22 00:00:13 -0800

    Audio Setup Stuff
    No actual sounds yet, but the code is there.

---
+++ Assets/_Scripts/AudioScriptsBelieve/PlaySoundOnTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnTimer : MonoBehaviour {
+
+    AudioSource audioSource;
+    public AudioClip[] ShipCreakSounds;
+    public float soundTimer;
+    float nextSoundTime;
+
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        nextSoundTime = Time.time + soundTimer;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time >= nextSoundTime)
+        {
+            nextSoundTime = Time.time + soundTimer;
+            SoundManager.Instance.PlayAudio(audioSource, ShipCreakSounds);
+        }
+	}
+}
---
+++ Assets/_Scripts/AudioScriptsBelieve/PlayerAudio.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip[] Jump;
+    public AudioClip[] Footsteps;
+    public AudioClip[] drowningSound;
+    public AudioClip Wave;
+    public AudioClip SpawnSound;
+    public AudioClip dieSound;
+    
+
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	public void PlayJumpSound()
+    {
+        SoundManager.Instance.PlayAudio(audioSource, Jump);
+    }
+
+    public void PlaySpawnSound()
+    {
+        SoundManager.Instance.PlayAudio(audioSource, SpawnSound);
+    }
+
+    public void PlayDieSounds()
+    {
+        SoundManager.Instance.PlayAudio(audioSource, dieSound);
+    }
+
+    public void PlayWaveSound()
+    {
+        SoundManager.Instance.PlayAudio(audioSource, Wave);
+    }
+
+    public void PlayFootsteps()
+    {
+        SoundManager.Instance.PlayAudio(audioSource, Footsteps);
+    }
+
+    public void PlayDrowningSound()
+    {
+        SoundManager.Instance.PlayAudio(audioSource, drowningSound);
+    }
+}
