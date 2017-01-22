a0b61fec9661c0c1b005a003f41cefb169127cac
Stralor <github@patrick-scott.com>
2017-01-22 13:56:03 -0800

    Testing IP saving

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+
+	public override void OnClientDisconnect(NetworkConnection conn)
+	{
+		base.OnClientDisconnect(conn);
+		print("Client Disconnect");
+
+		var ipField = GameObject.Find("Host Address Field");
+		if (ipField != null)
+		{
+			ipField.GetComponent<UnityEngine.UI.InputField>().text = conn.address;
+			print("IP saved");
+		}
+	}
+
+}
